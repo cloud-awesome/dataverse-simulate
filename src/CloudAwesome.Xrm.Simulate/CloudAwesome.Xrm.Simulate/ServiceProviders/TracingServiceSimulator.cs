ccce6260b5a02ed468bef659e41e0ce58749c17d
Arthur Nicholson-Gumuła <42772282+apng1982@users.noreply.github.com>
2026-09-16 17:50:19 +0100

    Refactors ServiceProviderSimulator to use instance-scoped contexts (#385)

--- src/CloudAwesome.Xrm.Simulate/CloudAwesome.Xrm.Simulate/ServiceProviders/TracingServiceSimulator.cs
+++ src/CloudAwesome.Xrm.Simulate/CloudAwesome.Xrm.Simulate/ServiceProviders/TracingServiceSimulator.cs
@@ -1,4 +1,4 @@
-﻿using CloudAwesome.Xrm.Simulate.DataServices;
+using CloudAwesome.Xrm.Simulate.DataServices;
 using CloudAwesome.Xrm.Simulate.Interfaces;
 using Microsoft.Xrm.Sdk;
 using NSubstitute;
@@ -7,18 +7,15 @@ namespace CloudAwesome.Xrm.Simulate.ServiceProviders;
 
 public static class TracingServiceSimulator
 {
-    private static MockedLoggingService _mockedLoggingService = null!;
-    
-    public static ITracingService? Create(MockedEntityDataService dataService, MockedLoggingService loggingService, 
+    public static ITracingService? Create(MockedEntityDataService dataService, MockedLoggingService loggingService,
         ISimulatorOptions? options)
     {
         if (dataService.FakeServiceFailureSettings is { TracingService: true })
         {
             return null;
         }
-        
+
         var tracingService = Substitute.For<ITracingService>();
-        _mockedLoggingService = loggingService;
 
         tracingService
             .When(x => x.Trace(Arg.Any<string>(), Arg.Any<object[]>()))
@@ -26,7 +23,7 @@ public static class TracingServiceSimulator
             {
                 var trace = t.Arg<string>();
                 var args = t.Arg<object[]>();
-                _mockedLoggingService.Add(trace, args);
+                loggingService.Add(trace, args);
             });
 
         return tracingService;
